baa7edc179ef4b6bcc2be24472587f41c94af1a6
ADANI\zabrodin <rediskal50@gmail.com>
2025-06-23 17:56:15 +0300

    ref: refactoring settings

--- DeskAssistant/Services/EmailSettings.cs
+++ DeskAssistant/Services/EmailSettings.cs
@@ -2,12 +2,12 @@
 {
     public class EmailSettings
     {
-        public string EmailId { get; set; }
-        public string Name { get; set; }
-        public string Host { get; set; }
+        public string? EmailId { get; set; }
+        public string? Name { get; set; }
+        public string? Host { get; set; }
         public int Port { get; set; }
         public bool UseTLS { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
     }
 }
