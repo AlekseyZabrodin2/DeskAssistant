48b7624217fbdeddffbf3524fc9c6fc9b5df07c8
ADANI\zabrodin <rediskal50@gmail.com>
2025-10-28 20:16:53 +0300

    feat: create Task service for work with DB

---
+++ DeskAssistant/Services/TaskService.cs
@@ -0,0 +1,48 @@
+﻿using DeskAssistant.DataBase;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.ObjectModel;
+
+namespace DeskAssistant.Services
+{
+    public class TaskService
+    {
+        private readonly TasksDbContext _context;
+
+
+        public TaskService(TasksDbContext context)
+        {
+           _context = context;
+        }
+
+
+        public async Task<List<CalendarTaskEntity>> GetAllTasksAsync()
+        {
+            //_logger.Trace("Get All patients");
+
+            var response = await _context.Tasks.ToListAsync();
+            return response;
+        }
+
+        public async Task AddTaskForSelectedDateAsync()
+        {
+            var task = new CalendarTaskEntity 
+            { 
+                Name = "Обед",
+                Description = "комплекс № 5",
+                DueDate = DateTime.UtcNow.AddDays(9),
+                Priority = "Normal",
+                Category = "General",
+                IsCompleted = true,
+                Status = "Pending", 
+                Tags = "tag1",
+                RecurrencePattern = "None" 
+            };
+            
+            _context.Tasks.Add(task);
+            _context.SaveChanges();
+        }
+
+
+
+    }
+}
