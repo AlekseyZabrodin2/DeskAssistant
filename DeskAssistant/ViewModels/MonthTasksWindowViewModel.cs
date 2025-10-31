6331c3f4cbaaa8b08af8ec5b1515937a703b11b7
ADANI\zabrodin <rediskal50@gmail.com>
2025-10-31 22:11:58 +0300

    ref: create  month task viewmodel

---
+++ DeskAssistant/ViewModels/MonthTasksWindowViewModel.cs
@@ -0,0 +1,45 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using DeskAssistant.Models;
+using DeskAssistant.Views;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System.Collections.ObjectModel;
+
+namespace DeskAssistant.ViewModels
+{
+    public partial class MonthTasksWindowViewModel : ObservableObject
+    {
+
+        [ObservableProperty]
+        public partial ObservableCollection<CalendarTaskModel> MonthTasksAllTasks { get; set; }
+
+        
+
+        public async Task OpenTaskDetails(CalendarTaskModel task)
+        {
+            var dialogVm = new AddTaskDialogViewModel
+            {
+                DialogName = task.Name,
+                DialogDescription = task.Description,
+                DialogDueDate = task.DueDate,
+                DialogPriority = task.Priority,
+                DialogCategory = task.Category,
+                IsDoubleTapped = true,
+                IsComboBoxDropdown = false
+            };
+
+            var dialogControl = new AddTaskDialogControl(dialogVm);
+            var dialog = new ContentDialog
+            {
+                Title = "Добавить задачу",
+                CloseButtonText = "Закрыть",
+                DefaultButton = ContentDialogButton.Primary,
+                Content = dialogControl,
+                XamlRoot = App.MainWindow.Content.XamlRoot
+            };
+
+            var result = await dialog.ShowAsync();
+        }
+    }
+}
