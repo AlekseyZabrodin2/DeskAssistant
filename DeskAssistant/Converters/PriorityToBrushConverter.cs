42d3330aa25e6bbfb6af13b9fff820dd63e7d915
ADANI\zabrodin <rediskal50@gmail.com>
2025-10-31 22:13:01 +0300

    feat: create converters

---
+++ DeskAssistant/Converters/PriorityToBrushConverter.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.UI;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Media;
+
+namespace DeskAssistant.Converters
+{
+    public class PriorityToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is PrioritiesLevel priority)
+            {
+                switch (priority)
+                {
+                    case PrioritiesLevel.Высокий:
+                        return new SolidColorBrush(Colors.Red);
+
+                    case PrioritiesLevel.Средний:
+                        return new SolidColorBrush(Colors.Orange);
+
+                    case PrioritiesLevel.Низкий:
+                        return new SolidColorBrush(Colors.YellowGreen);
+
+                    default:
+                        return new SolidColorBrush(Colors.Gray);
+                }
+
+            }
+            return new SolidColorBrush(Colors.Transparent);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+    }
+}
---
+++ DeskAssistant/Converters/StatusColorConverter.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.UI;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Media;
+
+namespace DeskAssistant.Converters
+{
+    public class StatusColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is TaskStatus status)
+            {
+                switch (status)
+                {
+                    case TaskStatus.Pending:
+                        return new SolidColorBrush(Colors.Orange);
+
+                    case TaskStatus.InProgress:
+                        return new SolidColorBrush(Colors.Green);
+
+                    case TaskStatus.Completed:
+                        return new SolidColorBrush(Colors.LightSkyBlue);
+
+                    default:
+                        return new SolidColorBrush(Colors.Gray);
+                }
+                    
+            }
+            return new SolidColorBrush(Colors.Transparent);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+    }
+}
