6c39a40cec07584cc2334752616e9cf83dda7f18
Aleksey Zabrodin <alekseyzabrodin2.0@gmail.com>
2025-11-01 22:58:49 +0300

    ref: create frame with user controls

--- DeskAssistant/Views/CalendarPage.xaml.cs
+++ DeskAssistant/Views/CalendarPage.xaml.cs
@@ -1,10 +1,6 @@
-using DeskAssistant.Models;
+using DeskAssistant.Services;
 using DeskAssistant.ViewModels;
-using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Shapes;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -18,129 +14,17 @@ namespace DeskAssistant.Views
     {
         public CalendarViewModel ViewModel { get; }
 
+
         public CalendarPage(CalendarViewModel viewModel)
         {
             this.InitializeComponent();
+
             ViewModel = viewModel;
-            DataContext = ViewModel;
-            
-            ViewModel.TasksUpdated += () =>
-            {
-                DispatcherQueue.TryEnqueue(UpdateAllCalendarDots);
-            };
-        }
 
+            var tasksControl = new CalendarUserControl(ViewModel);
+            CalendarFrame.Content = tasksControl;
 
-
-        private void CalendarView_SelectedDatesChanged(CalendarView sender, CalendarViewSelectedDatesChangedEventArgs args)
-        {
-            if (sender.SelectedDates.Any())
-            {
-                var selectedDate = sender.SelectedDates.First();
-                ViewModel.SelectedDate = DateOnly.FromDateTime(selectedDate.DateTime);
-
-                ViewModel.GetTasksForSelectedDate();
-            }
-        }
-
-        private void CalendarView_Loaded(object sender, RoutedEventArgs e)
-        {
-            ViewModel.TasksUpdated += () =>
-            {
-                DispatcherQueue.TryEnqueue(() => UpdateAllCalendarDots());
-            };
-
-            UpdateAllCalendarDots();
-        }
-
-        private void UpdateAllCalendarDots()
-        {
-            CalendarViewContainer.UpdateLayout();
-
-            var dayItems = FindChildren<CalendarViewDayItem>(CalendarViewContainer);
-            foreach (var dayItem in dayItems)
-            {
-                UpdateDotForDayItem(dayItem);
-            }
-        }
-
-        // Вспомогательный метод для обновления одной даты
-        private void UpdateDotForDayItem(CalendarViewDayItem dayItem)
-        {
-            var date = DateOnly.FromDateTime(dayItem.Date.DateTime);
-
-            bool hasTasks = ViewModel.AllTasks.Any(t => t.DueDate == date && !t.IsCompleted);
-
-            var taskDot = FindChild<Ellipse>(dayItem, "TaskDot");
-            if (taskDot != null)
-                taskDot.Visibility = hasTasks ? Visibility.Visible : Visibility.Collapsed;
-        }
-
-        public static IEnumerable<T> FindChildren<T>(DependencyObject parent) where T : DependencyObject
-        {
-            if (parent == null) yield break;
-
-            int count = VisualTreeHelper.GetChildrenCount(parent);
-            for (int i = 0; i < count; i++)
-            {
-                var child = VisualTreeHelper.GetChild(parent, i);
-
-                if (child is T t) yield return t;
-
-                foreach (var descendant in FindChildren<T>(child))
-                    yield return descendant;
-            }
-        }
-
-        private void CalendarView_CalendarViewDayItemChanging(CalendarView sender, CalendarViewDayItemChangingEventArgs args)
-        {
-            var date = DateOnly.FromDateTime(args.Item.Date.DateTime);
-
-            // Проверяем, есть ли задачи на эту дату
-            bool hasTasks = ViewModel.AllTasks.Any(t =>
-                t.DueDate == date && !t.IsCompleted);
-
-            // Находим элемент TaskDot в визуальном дереве
-            if (args.Phase == 0 && args.Item is CalendarViewDayItem dayItem)
-            {
-                var taskDot = FindChild<Ellipse>(dayItem, "TaskDot");
-                if (taskDot != null)
-                {
-                    taskDot.Visibility = hasTasks ? Visibility.Visible : Visibility.Collapsed;
-
-                    // Меняем цвет точки по приоритету
-                    if (hasTasks)
-                    {
-                        var highPriorityTasks = ViewModel.AllTasks.Any(t =>
-                            t.DueDate == date && !t.IsCompleted );
-                    }
-                }
-            }
-        }
-
-        // Вспомогательный метод для поиска в визуальном дереве
-        private T FindChild<T>(DependencyObject parent, string childName) where T : DependencyObject
-        {
-            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++)
-            {
-                var child = VisualTreeHelper.GetChild(parent, i);
-
-                if (child is T foundElement && (child as FrameworkElement)?.Name == childName)
-                    return foundElement;
-
-                var result = FindChild<T>(child, childName);
-                if (result != null)
-                    return result;
-            }
-            return null;
-        }
-
-        private void ListView_DoubleTapped(object sender, DoubleTappedRoutedEventArgs e)
-        {
-            if (sender is ListView listView && listView.SelectedItem is CalendarTaskModel selectedTask)
-            {
-                ViewModel.OpenTaskDetails(selectedTask);
-            }
+            ViewModel.InitializeFrame(CalendarFrame);
         }
     }
 }
