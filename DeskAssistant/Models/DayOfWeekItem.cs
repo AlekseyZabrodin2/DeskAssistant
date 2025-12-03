699650cedb2ac93e6d1a0f6fcb3ff71a30eb0f2e
ADANI\zabrodin <rediskal50@gmail.com>
2025-12-03 15:17:22 +0300

    feat: create DayOfWeekItem model

---
+++ DeskAssistant/Models/DayOfWeekItem.cs
@@ -0,0 +1,13 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DeskAssistant.Models
+{
+    public partial class DayOfWeekItem : ObservableObject
+    {
+        [ObservableProperty]
+        public partial string Name { get; set; }
+
+        [ObservableProperty]
+        public partial bool IsChecked { get; set; }
+    }
+}
